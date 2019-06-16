c9fd1563c115ec22341ec0138a6a6de91ecac67f
Paul Beckwith <303dog@gmail.com>
2019-06-16 04:35:43 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,3 +1,7 @@
-def #meal_choice (veg1, veg2)
+def Meal_Choice (#{veg1}, #{veg2}, #{protein})
   veg1 = broccoli
   veg2 = squash
+  protein = beef
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}"
+end
