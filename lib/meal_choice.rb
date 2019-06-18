10294dad045f4a82c9c49d524663df6f010bfc99
salubrious-pipe-1423 <digregoriothomas@yahoo.com>
2019-06-18 16:30:25 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,10 @@
-# Your code here
+[1] pry(main)> def meal_choice( meal = "meat")
+[1] pry(main)*   puts "#{meal}" 
+[1] pry(main)* end  
+#=> :meal_choice
+[2] pry(main)> meal_choice
+#=> meat
+#=> nil
+[3] pry(main)> meal_choice(:sdgsdg)
+#=> sdgsdg
+#=> nil
