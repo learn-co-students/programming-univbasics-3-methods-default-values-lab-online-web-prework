af6bbad391ee5cc1687b22c50dede08391787be5
Dominique De León <dominique.a.deleon@gmail.com>
2019-06-22 08:00:40 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,6 @@
 # Your code here
+def meal_choice(option1, option2, option3="meat")
+  puts "What a nutritious meal!"
+  puts "A plate of #{option3} with #{option1} and #{option2}."
+  "A plate of #{option3} with #{option1} and #{option2}."
+end
