bb29579bba4c2f24c11708b9fc47cc6a56da85cd
Julie Cook <juliecook328@gmail.com>
2019-07-01 02:09:04 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,6 @@
 def meal_choice(veg1, veg2, protein = "meat")
   puts "What a nutritious meal!"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  order = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts order
+  order
 end 
