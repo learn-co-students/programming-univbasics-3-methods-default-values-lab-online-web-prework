8ad5456bfe27a5e0af911cc6ee7e89c1d1bbcb7b
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-21 22:19:35 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,2 @@
-# Your code here
+expect (meal_choice("brocolli","macaroni")).to eq("A plate of meat with broccoli and macaroni.")
+expect (meal_choice("broccoli", "macaroni",tofu")).to eq("A plate of tofu with broccoli and macaroni.")
