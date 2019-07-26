c326991ec9c28c170f20a785b6f79c8eab4f06d4
clean-command-1851 <ed7901979t@gmail.com>
2019-07-26 20:16:35 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,11 @@
-# Your code here
+# Edward Tomachevskiy
+# new program Meal Choice
+def meal_choice(veg1, veg2, protein="meat")
+  puts "What a nutritious meal!" 
+   
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
+
+end
+meal_choice("tomoto", "onion", "fat")
