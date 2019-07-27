8b4ba3d91c967ea551b1eed7c62dcb1c19df66b1
Michael Kofron <kofronmichael@gmail.com>
2019-07-27 02:26:50 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,9 @@
-# Your code here
+spec_helper
+
+def meal_choice(veg1, veg2, protein = "meat")
+  puts "What a nutritious meal!"
+ puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+
+  
