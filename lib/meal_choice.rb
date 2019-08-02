0d65f401ad43455ee30f2e44df6fe32a53d620a7
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-02 23:47:33 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,4 +2,5 @@
 
 '#meal_choice'
 it 'should default to meat for the protein'
- expect(meal_choice("broccoli", "macaroni" )).to eq("a plate of meat with broccoli and macaroni")
+ expect(meal_choice("broccoli", "macaroni" )).to eq("a plate of meat with broccoli and macaroni.")
+ 
