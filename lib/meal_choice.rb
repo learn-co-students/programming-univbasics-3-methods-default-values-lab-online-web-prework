000e75ce4a97216fd7631c45c0472a2a05f7196a
Samuel Owusu <kwadwoamo10@gmail.com>
2019-08-07 18:58:37 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,6 @@
-# Your code here
+meal_choice 
+puts "What a nutritious meal!"
+puts "A plate of #{protein} with #{veg1} && #{veg2}"
+return nil
+puts "A plate of #{protein} with #{veg1} && #{veg2}"
+return nil 
