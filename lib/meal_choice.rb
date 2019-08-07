8987f56daa941a64674091afa820b76c3582ccad
Samuel Owusu <kwadwoamo10@gmail.com>
2019-08-07 20:41:52 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,12 @@
-meal_choice 
-puts "What a nutritious meal!"
-puts "A plate of #{protein} with #{veg1} && #{veg2}"
-return nil
-puts "A plate of #{protein} with #{veg1} && #{veg2}"
-return nil 
+def method (meal_choice)
+  
+  puts "What a nutritious meal!#{protein,veg1,veg2}"
+  
+  puts "A plate of #{protein} with #{veg1} and #{veg2}
+  
+  puts "meal_choice #{veg1}"
+end
+  put meal_choice #{veg2}
+  
+puts A plate of #{protein}
+return meal_choice
