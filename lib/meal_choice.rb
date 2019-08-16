79ceea593f4ab6caf6edf0b8369c9134cfd4a1aa
Charity Parks <tuppercharity@gmail.com>
2019-08-16 18:54:43 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,11 @@
-# Your code here
+ def meal_choice(veg1, veg2, protein = "meat")
+puts "What a nutritious meal!"
+puts "A plate of #{protein} with #{veg1} and #{veg2}."
+"A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+# It takes 2 veggies and protein defaults to meat
+# It returns `meal` with a value of
+# "A plate of #{protein} with #{veg1} and #{veg2}"
+# and `puts` "What a nutritious meal!"
+#eating, so make sure the return value includes the **three** food items you've chosen by returning: "A plate of #{protein} with #{veg1} and #{veg2}."
