0454b8aafccfd15b1193b0c829f976f823437f41
Alina Kornilova <alinakornilova78@gmail.com>
2019-08-24 23:54:53 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,8 @@
-# Your code here
+def meal_choice(veg1,veg2,protein="meat")
+  veg1="broccoli"
+  veg2="macaroni"
+  puts "What a nutritious meal!"
+  return "A plate of #{meat} with #{veg1} and #{veg2}."
+end
+
+return meal_choice((veg1,veg2,protein="meat")
