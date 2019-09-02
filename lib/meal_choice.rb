3e9b55c2e05a8c4671cf4810b42c7f602cc01899
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-02 03:31:22 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,6 @@
 # Your code here
 def meal_choice (veg1, veg2, protein = "meat")
-  p "What a nutritious meal!"
-  p "A plate of #{protein} with #{veg1} and #{veg2}."
+  p"What a nutritious meal!"
+  #puts " A plate of #{protein} with #{veg1} and #{veg2}."
+  p " A plate of #{protein} with #{veg1} and #{veg2}."
 end
