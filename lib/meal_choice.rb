a8b3225b6059da81bb96bd85d1cdf254e950f716
Jordan Hayes <nosafoundation@gmail.com>
2019-09-12 00:23:58 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,7 @@
 # Your code here
+if meal_choice(veg1, veg2, protein = 'meat')
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts "What a nutritious meal!"
+end 
+meal_choice("broccoli", "macaroni")
+meal_choice("broccoli", "macaroni", "tofu")
