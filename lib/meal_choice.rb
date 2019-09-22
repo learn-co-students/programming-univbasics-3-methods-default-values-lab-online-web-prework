c85e3a4906ca3a84bf20ea90c947cf083e4956e1
William <teststuffreally@gmail.com>
2019-09-22 14:21:52 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,3 +1,5 @@
-def
-puts meal_choice "A plate of #{protein}with#{veg1}and#{veg2}."
+def meal_choice (veg1, veg2, protein="meat")
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
