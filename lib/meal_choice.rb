e01a9eae2082a279c0e9382973e829a9e32b01e7
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-09-24 22:58:19 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,13 @@
-# Your code here
+
+def meal_choice (veg1 = "broccoli" , veg2 = "macaroni", protien = "meat",)
+  p "A plate of #{protien} with #{veg1} and #{veg2}."
+end
+
+ 
+def meal_choice (veg1 = "broccoli" , veg2 = "macaroni", protien = "tofu")
+  p "A plate of #{protien} with #{veg1} and #{veg2}."
+end 
+
+if meal_choice == "A plate of meat with broccoli and macaroni."
+  p "What a nutritious meal!"
+end
