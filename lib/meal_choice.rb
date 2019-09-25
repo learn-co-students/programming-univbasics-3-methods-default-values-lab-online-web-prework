1e717e92c9d21d82012fa8ebc7df1c78c216e631
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-25 08:55:55 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,6 @@
-# Your code here
+"What a nutritious meal!"
+def veg1 = "Corn"
+def veg2 = "broccoli"
+def protein = "meat"
+  it 'What a nutritious meal!'
+   it 'A plate of #{protein} with #{veg1} and #{veg2}.'
