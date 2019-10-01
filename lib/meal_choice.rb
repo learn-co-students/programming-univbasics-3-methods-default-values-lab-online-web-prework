fe6b81846d1a9531576990dbf3d9bf276c3860a9
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2019-10-01 02:10:54 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,9 @@
-# Your code here
+def 'meal_choice' do
+  it 'should default to meat for the protein' do
+    expect(meal_choice("carrots", "bokchoy")).to eq("A plate of meat with carrots and bokchoy.")
+  end
+  
+  it 'should allow you to set a protein' do
+    expect(meal_choice("carrots", "bokchoy")).
+  it 'should puts "What a nutritious meal!" and your order to the console' do
+end
