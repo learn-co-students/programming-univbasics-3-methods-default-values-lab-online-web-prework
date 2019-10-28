0a6251f1b7b79a9bf2e4c7604c948468aa3f90df
connor worthen <connorw220@protonmail.com>
2019-10-28 07:37:08 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,6 @@
-# Your code here
+def meal_choice(veg1, veg2, protein*) #with optional parameter
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+p "A plate of #{meat} with #{kale} and #{spinach}"
