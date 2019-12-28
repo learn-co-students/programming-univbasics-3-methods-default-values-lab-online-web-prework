9f35dc632ea84498c80963d3613b9e431f5aa60d
Erik Prenzlin <eprenzlin@gmail.com>
2019-12-28 14:51:33 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,10 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+
+def meal_choice(veg1,veg2, protein = "meat")
+  puts " What a nutritious meal!" 
+  meal_confirm= " A plate of #{protein}, with #{veg1} and #{veg2}."
+  puts meal_confirm
+end 
+
+meal_choice(veg1, veg2, protein)
---
+++ play.rb
@@ -0,0 +1,10 @@
+def greeting (name, country,  language= "Ruby")
+  puts "Hi, #{name}, #{country},  #{language}"
+  boom = "Glory to #{country}"
+  puts boom
+end
+
+greeting("Erik", "HK", "Python")
+
+
+
